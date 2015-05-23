2de85452e93555dea5a3649657fff3a0be5dfd7c
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-05-23 15:47:59 +0200

    create Dots.

---
+++ Barnes-Hut-Algorithm/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+	public GameObject dot;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Vector3 mouse = Input.mousePosition;
+			mouse = Camera.main.ScreenToWorldPoint(mouse);
+			mouse.z = 0;
+			Instantiate(dot,  mouse, Quaternion.identity);
+		}
+	}
+}
