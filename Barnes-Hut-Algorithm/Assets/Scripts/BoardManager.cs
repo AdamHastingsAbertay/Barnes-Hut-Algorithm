ebb41b23297476bfca3dbfe03c21793681084149
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-05-25 13:11:00 +0200

    Refacto

--- Barnes-Hut-Algorithm/Assets/Scripts/BoardManager.cs
+++ Barnes-Hut-Algorithm/Assets/Scripts/BoardManager.cs
@@ -15,10 +15,6 @@ public class BoardManager : MonoBehaviour {
 	private System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
 
 	void Start(){
-		 Vector3 a = new Vector3(1, 2, 3);
-		 Vector3 b = new Vector3(4, 3, 2);
-	
-			print(Vector3.Min(a, b));
 		compute =true;
 		circle = false;
 		size = bodys.Count;
@@ -27,36 +23,47 @@ public class BoardManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
-		if(Input.GetKeyDown("space"))
-			compute = !compute;
-
-		if (Input.GetButtonDown("Fire1")) {
-			Vector3 mouse = Input.mousePosition;
-			mouse = Camera.main.ScreenToWorldPoint(mouse);
-			mouse.z = 0;
-			GameObject dotGO = Instantiate(dot,  mouse, Quaternion.identity) as GameObject;
-			bodys.Add(new Body(dotGO));
-			size = bodys.Count;
-		}
-		framecount++;
-		if(circle && framecount%10 ==0 ){
-			GameObject dotGO = Instantiate(dot,  new Vector3(Mathf.Cos(Time.time)*10,Mathf.Sin(Time.time)*10,0), Quaternion.identity) as GameObject;
-			bodys.Add(new Body(dotGO));
-			size = bodys.Count;
-		}
+		createBodyIfNeeded ();
 
 		if(compute){
-			stopwatch.Start()	;
-			foreach(Body bodyFirst in bodys){
-				foreach(Body bodySecond in bodys){
-					bodyFirst.interac(bodySecond);
-				}
-				bodyFirst.update();
-			}
+			stopwatch.Start();
+
+			bruteFroceUpdate ();
+
 			stopwatch.Stop();
 			print("plop "+size+";"+stopwatch.ElapsedTicks);
 			stopwatch.Reset();
+		}
+	}
+
+	void bruteFroceUpdate ()
+	{
+		foreach (Body bodyFirst in bodys) {
+			foreach (Body bodySecond in bodys) {
+				bodyFirst.interac (bodySecond);
+			}
+			bodyFirst.update ();
+		}
+	}
+
+	private void createBodyIfNeeded ()
+	{
+		if(Input.GetKeyDown("space"))
+			compute = !compute;
+
+		if (Input.GetButtonDown ("Fire1")) {
+			Vector3 mouse = Input.mousePosition;
+			mouse = Camera.main.ScreenToWorldPoint (mouse);
+			mouse.z = 0;
+			GameObject dotGO = Instantiate (dot, mouse, Quaternion.identity) as GameObject;
+			bodys.Add (new Body (dotGO));
+			size = bodys.Count;
+		}
+		framecount++;
+		if (circle && framecount % 10 == 0) {
+			GameObject dotGO = Instantiate (dot, new Vector3 (Mathf.Cos (Time.time) * 10, Mathf.Sin (Time.time) * 10, 0), Quaternion.identity) as GameObject;
+			bodys.Add (new Body (dotGO));
+			size = bodys.Count;
 		}
 	}
 }
