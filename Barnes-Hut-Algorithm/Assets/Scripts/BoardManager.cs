2eae2056b750e2f56c731c2a3904423e9d190e34
AdamHastingsAbertay <46451931+AdamHastingsAbertay@users.noreply.github.com>
2021-01-26 14:56:47 +0000

    Addition of things on screen and code changes
    Extra Changes to add some display on screen

--- Barnes-Hut-Algorithm/Assets/Scripts/BoardManager.cs
+++ Barnes-Hut-Algorithm/Assets/Scripts/BoardManager.cs
@@ -1,74 +1,89 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using Unity;
+using UnityEngine.UI;
 
 
 public class BoardManager : MonoBehaviour
 {
 
-	public GameObject dot;
+    public GameObject dot;
 
-	private List<Body> bodys = new List<Body> ();
-	private bool compute;
-	private bool displayQuad;
-	private bool circle;
-	private bool bruteForce;
-	private int size;
-	private double framecount = 0;
-	private Boundary boundary;
-	private System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch ();
-	private QuadNode quadTree;
-	private float dist;
-	private List<Quad> quads = new List<Quad> ();
+    public List<Body> bodys = new List<Body>();
+    private bool compute;
+    public bool displayQuad;
+    private bool circle;
+    public bool bruteForce;
+    public int size;
+    private double framecount = 0;
+    private Boundary boundary;
+    private System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+    public QuadNode quadTree;
+    private float dist;
+    public List<Quad> quads = new List<Quad>();
+    public float Theta = 0.5f;
 
-	void Start ()
-	{
-		boundary = new Boundary (1000);
-		dist = 0;
-		compute = false;
-		circle = false;
-		bruteForce = false;
-		displayQuad = true;
-		size = bodys.Count;
-		Debug.Log ("##### &start =#####");
-		for(int i = -10;i<10;i++){
-			for(int j = -10;j<10;j++){
-				GameObject dotGO = Instantiate (dot, new Vector3 (i*2, j*2, 0), Quaternion.identity) as GameObject;
-				bodys.Add (new Body (dotGO));
-			}
-		}
+    public Text NodeCount;
+    public Text ThetaCount;
 
-	}
+    void Start()
+    {
+        boundary = new Boundary(1000);
+        dist = 0;
+        compute = false;
+        circle = false;
+        bruteForce = false;
+        displayQuad = true;
+        Debug.Log("##### &start =#####");
+        for (int i = -5; i < 5; i++) {
+            for (int j = -5; j < 5; j++) {
+                GameObject dotGO = Instantiate(dot, new Vector3(i * 2, j * 2, 0), Quaternion.identity) as GameObject;
+                bodys.Add(new Body(dotGO));
+            }
+        }
+        size = bodys.Count;
+        NodeCount.text = "Node Count: " + size;
 
-	// Update is called once per frame
-	void Update ()
-	{
-		createBodyIfNeeded ();
+    }
 
-		boundary.update (bodys);
-		float sized = Mathf.Max ((boundary.max.x - boundary.min.x), (boundary.max.y - boundary.min.y));
-		Vector3 center = new Vector3 ((boundary.max.x + boundary.min.x) / 2, (boundary.max.y + boundary.min.y) / 2, (boundary.max.z + boundary.min.z) / 2);
-		quadTree = new QuadNode (1, center, sized);
+    // Update is called once per frame
+    void Update()
+    {
+        createBodyIfNeeded();
 
-		stopwatch.Start();
-		foreach (Body bod in bodys) {
-			quadTree.addBody (bod);
-		}
-		if (compute) {
-		
-			if(bruteForce)
-				bruteFroceUpdate();
-			else
-				BarnesHut ();
-		}
+        boundary.update(bodys);
+        float sized = Mathf.Max((boundary.max.x - boundary.min.x), (boundary.max.y - boundary.min.y));
+        Vector3 center = new Vector3((boundary.max.x + boundary.min.x) / 2, (boundary.max.y + boundary.min.y) / 2, (boundary.max.z + boundary.min.z) / 2);
+        quadTree = new QuadNode(1, center, sized);
+
+        stopwatch.Start();
+        foreach (Body bod in bodys) {
+            quadTree.addBody(bod);
+        }
+        if (compute)
+        { }
+
+        if (bruteForce)
+        {
+            bruteFroceUpdate();
+            ThetaCount.text = "θ Value: 0";
+        }
+        else
+        {
+            BarnesHut();
+            ThetaCount.text = "θ Value: " + Theta;
+        }
 		stopwatch.Stop();
-		Debug.Log("plop "+bodys.Count+";"+stopwatch.ElapsedTicks);
+		//Debug.Log("plop "+bodys.Count+";"+stopwatch.ElapsedTicks);
 		stopwatch.Reset();
 	}
 
 	void BarnesHut(){
 		foreach (Body body in bodys){
-			quadTree.interact(body,0.5f);
+			quadTree.interact(body,Theta);
+
 			body.update();
 		}
 	}
@@ -103,14 +118,18 @@ public class BoardManager : MonoBehaviour
 			GameObject dotGO = Instantiate (dot, mouse, Quaternion.identity) as GameObject;
 			bodys.Add (new Body (dotGO));
 			size = bodys.Count;
-		}
+
+            NodeCount.text = "Node Count: " + size;
+        }
 		framecount++;
 		if (circle) {
 			GameObject dotGO = Instantiate (dot, new Vector3 (Mathf.Cos (Time.time) * dist, Mathf.Sin (Time.time) * dist, 0), Quaternion.identity) as GameObject;
 			bodys.Add (new Body (dotGO));
 			dist+=0.05f;
 			size = bodys.Count;
-		}
+
+            NodeCount.text = "Node Count: " + size;
+        }
 	}
 
 	public void OnDrawGizmos ()
@@ -120,8 +139,8 @@ public class BoardManager : MonoBehaviour
 			foreach (Quad quad in quads) {
 				Gizmos.color = quad.color;
 				Gizmos.DrawWireCube (quad.position, quad.size);
-				if(quad.gravityCenter != Vector3.zero)
-		 			Gizmos.DrawSphere(quad.gravityCenter,quad.mass);
+				if(quad.gravityCenter != Vector3.zero) { } 
+		 			//Gizmos.DrawSphere(quad.gravityCenter,quad.mass);
 			}
 			quads.Clear ();
 		}
---
+++ Barnes-Hut-Algorithm/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public float speed = 30f;
+    private Vector3 move = new Vector3();
+    public Camera MainCamera;
+
+    // Use this for initialization
+    void Start () {
+        MainCamera = GetComponent<Camera>();
+        MainCamera.orthographicSize = 100;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        move.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        move.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+
+        //move.z = Input.GetAxis("Lateral") * speed * Time.deltaTime;
+
+        //this.GetComponent<Camera>().orthographicSize = Input.GetAxis("Lateral") * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Q)) // Change From Q to anyother key you want
+        {
+            MainCamera.orthographicSize = MainCamera.orthographicSize + speed * Time.deltaTime;
+          
+        }
+
+        if (Input.GetKey(KeyCode.E)) // Change From Q to anyother key you want
+        {
+            MainCamera.orthographicSize = MainCamera.orthographicSize - speed * Time.deltaTime;
+
+        }
+
+
+        //MainCamera.orthographicSize = Input.GetAxis("Lateral") * MainCamera.orthographicSize * Time.deltaTime;
+        //Input.GetAxis("Lateral") * MainCamera.orthographicSize + 1 * Time.deltaTime;
+
+        if (MainCamera.orthographicSize > 500)
+        {
+            MainCamera.orthographicSize = 500; // Max size
+        }
+
+        if (MainCamera.orthographicSize < 25)
+        {
+            MainCamera.orthographicSize = 25; // Max size
+        }
+
+        move = transform.TransformDirection(move);
+        transform.position += move;
+    }
+}
